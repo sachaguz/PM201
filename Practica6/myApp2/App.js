6dfb1486a1cf588ba873af865b7d63d22baf278e
Saul Alejandro <sachaguz05@gmail.com>
2026-06-08 18:58:46 -0600

    Práctica 6. Trabajo en clase

---
+++ Practica6/myApp2/App.js
@@ -0,0 +1,38 @@
+/* Zona 1: Importaciones de archivos y componentes*/
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Saludo } from './components/Saludo';
+import { Saludo2 } from './components/Saludo2';
+
+/* Zona 2: Main - Componentes  */
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      <Image source={require('./assets/wave.png')} />
+      <Text>Hola mundo React Native</Text>
+      <Text>-----------------------------------------------------------------------------------</Text>
+      <Saludo/>
+
+      <Text>-----------------------------------------------------------------------------------</Text>
+      <Saludo2/>
+
+      <StatusBar style="auto" />
+      
+
+    </View>
+  );
+}
+
+
+/* Zona 3: Estilos y posicionamiento de los componentes*/
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
